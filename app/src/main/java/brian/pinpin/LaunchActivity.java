67ad7746e68144d3922b90d66d76ce86bc7ec53e
Xiaochen <neteasy1210@gmail.com>
2016-12-22 22:49:37 +1300

    clean code

--- app/src/main/java/brian/pinpin/LaunchActivity.java
+++ app/src/main/java/brian/pinpin/LaunchActivity.java
@@ -9,9 +9,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.Display;
+import android.view.WindowManager.LayoutParams;
 import android.widget.Button;
 
-//import brian.pinpin.scenes.PlayScene;
+import brian.pinpin.scenes.PlayScene;
 import org.cocos2d.layers.CCScene;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCNode;
@@ -22,37 +23,33 @@ import brian.pinpin.managers.SceneManager;
 import brian.pinpin.managers.SoundManager;
 
 public class LaunchActivity extends Activity implements OnClickListener {
-    private CCGLSurfaceView mSurfaceView;
     private ProgressDialog mDialog;
-    private Button mExitBtn;
-    private Button mBackBtn;
-
     private SceneManager mSceneManager = (SceneManager) ManagerService.getInstance().getService(ManagerService.SCENE_MANAGER);
     private SoundManager mSoundManager = (SoundManager) ManagerService.getInstance().getService(ManagerService.SOUND_MANAGER);
 
     private void showExit() {
-        this.mDialog = new ProgressDialog(this);
+        mDialog = new ProgressDialog(this);
         View view = LayoutInflater.from(this).inflate(R.layout.exit_dialog, null);
-        this.mExitBtn = (Button)view.findViewById(R.id.exit_ok_btn);
-        this.mExitBtn.setOnClickListener(this);
-        this.mBackBtn = (Button)view.findViewById(R.id.exit_back_btn);
-        this.mBackBtn.setOnClickListener(this);
-        this.mDialog.show();
-        this.mDialog.setContentView(view);
+        Button exit = (Button)view.findViewById(R.id.exit_ok_btn);
+        exit.setOnClickListener(this);
+        Button back = (Button)view.findViewById(R.id.exit_back_btn);
+        back.setOnClickListener(this);
+        mDialog.show();
+        mDialog.setContentView(view);
     }
 
     public void onClick(View view) {
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.exit_ok_btn:
-                CCScene var2 = CCDirector.sharedDirector().getRunningScene();
-//                if(var2 != null && var2 instanceof PlayScene) {
-//                    ((PlayScene)var2).saveFinished();
-//                }
-                this.mDialog.dismiss();
-                this.finish();
+                CCScene scene = CCDirector.sharedDirector().getRunningScene();
+                if (scene instanceof PlayScene) {
+                    ((PlayScene)scene).saveFinished();
+                }
+                mDialog.dismiss();
+                finish();
                 return;
             case R.id.exit_back_btn:
-                this.mDialog.dismiss();
+                mDialog.dismiss();
                 return;
             default:
         }
@@ -60,25 +57,24 @@ public class LaunchActivity extends Activity implements OnClickListener {
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(1024, 1024);
-        this.getWindow().setFlags(128, 128);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(LayoutParams.FLAG_KEEP_SCREEN_ON, LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        Display display = this.getWindowManager().getDefaultDisplay();
+        Display display = getWindowManager().getDefaultDisplay();
         SceneManager.sceneWidth = display.getWidth();
         SceneManager.sceneHeight = display.getHeight();
 
-        mSurfaceView = new CCGLSurfaceView(this);
-        setContentView(mSurfaceView);
+        CCGLSurfaceView surfaceView = new CCGLSurfaceView(this);
+        setContentView(surfaceView);
         CCDirector.sharedDirector().setLandscape(true);
-        CCDirector.sharedDirector().attachInView(mSurfaceView);
+        CCDirector.sharedDirector().attachInView(surfaceView);
         CCDirector.sharedDirector().setDisplayFPS(false);
-        CCDirector.sharedDirector().setAnimationInterval(0.01666666753590107D);
         CCDirector.sharedDirector().runWithScene(mSceneManager.getScene(SceneManager.SCENE_FLASH));
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
             showExit();
             return true;
         } else {
